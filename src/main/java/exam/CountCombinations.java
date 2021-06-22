a384b3325892e162ba1831df8a0723d975bcf802
Jshajal <86229035+Jshajal@users.noreply.github.com>
2021-06-22 00:26:50 -0700

    Update CountCombinations.java

--- src/main/java/exam/CountCombinations.java
+++ src/main/java/exam/CountCombinations.java
@@ -1,10 +1,52 @@
-package exam;
+import java.io.*;
+ 
+class GFG
+{
+     
 
-public class CountCombinations {
+static int factorial(int n)
+{
+    int f = 1;
+    if (n == 0|| n == 1)
+        return 1;
+    for (int i = 2; i <= n; i++)
+        f = f * i;
+    return f;
+}
+ 
 
-  public int getNumberOfWays(final int total, final int k) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return 0;
-  }
+static int getSum(int arr[], int n)
+{
+    
+    int fact = factorial(n);
+ 
+    
+    int digitsum = 0;
+    for (int i = 0; i < n; i++)
+        digitsum += arr[i];
+    digitsum *= (fact / n);
+ 
+    
+    int res = 0;
+    for (int i = 1, k = 1;
+             i <= n; i++)
+    {
+        res += (k * digitsum);
+        k = k * 10;
+    }
+ 
+    return res;
+}
+ 
 
+public static void main (String[] args)
+{
+ 
+    
+    int arr[] = {1, 2, 3};
+    int n = arr.length;
+     
+    
+    System.out.println(getSum(arr, n));
+}
 }
